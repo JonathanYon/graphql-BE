79aa3ab22c4262cb6ae9e7b9fa35d1d16a8a3503
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2022-08-03 21:04:25 +0200

    server now running

---
+++ src/index.ts
@@ -0,0 +1,45 @@
+
+import express from "express"
+import mongoose from "mongoose";
+import cors from "cors"
+import { ApolloServer, gql } from "apollo-server-express";
+
+import typeDefs from "./graphql/schema";
+import resolvers from "./graphql/resolvers";
+import { constant } from "./config/constant";
+
+
+const {MONGODB_URL} = constant
+const port = constant.PORT
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+
+const startServer = async () => {
+    const apolloServer = new ApolloServer({
+        typeDefs, resolvers, context: ({req}: any) => ({req})
+    })
+    await apolloServer.start()
+    apolloServer.applyMiddleware({app, cors: false})
+
+   
+        mongoose.set('debug', true)
+        mongoose.connect(MONGODB_URL)
+        mongoose.connection.on('connected', () => {
+            console.log("🍃 Successfully connected to mongo!");
+            app.listen(port, () => {
+                console.log(`server running on:  http://localhost:${port}`)
+                console.log(`server running at:  http${apolloServer.graphqlPath}/`)
+            })
+        })
+        mongoose.connection.on("error", (err) => {
+            console.log("MONGO ERROR🚩🚩: ", err);
+          });
+   
+}
+startServer()
+
+
