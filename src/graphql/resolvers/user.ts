1539bb88f28ac25c24b717f1c1350094e4cec070
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2022-08-04 11:24:46 +0200

    updated signup resolver

--- src/graphql/resolvers/user.ts
+++ src/graphql/resolvers/user.ts
@@ -1,10 +1,14 @@
-import { Types } from "mongoose";
+import bcrypt from 'bcrypt'
+
 import { UserModel } from "../../models/users";
+import passwordValidate from "../../utils/passwordValidate";
+import { newToken } from '../../utils/token';
 
 type UserInput = {
-  input: {
+  userSignUpInput: {
     username: string;
     password: string;
+    confirmPassword?: string
   };
 };
 
@@ -25,20 +29,35 @@ export const userResolvers = {
   },
   Mutation: {
     // register: async(_: unknown, {username, password}: UserInput) => {
-    register: async (
+      signUp: async (
       _: any,
-      {input}: UserInput,
-      ctx: any
+      {userSignUpInput}: UserInput
     ) => {
       try {
-        const user = new UserModel({ username: input.username, password: input.password });
-        console.log("user--->", user);
-        const savedUser = await user.save();
-        console.log("parent--->", ctx);
+        const user = await UserModel.findOne({username: userSignUpInput.username})
+        if(user){
+          throw new Error('username is taken choose unique one')
+        }
+        if(!passwordValidate(userSignUpInput.password)){
+          throw new Error("Password doesn't meet password requirements.")
+        }
+        if(userSignUpInput.password !== userSignUpInput.confirmPassword){
+          throw new Error("Password and confirm password don't match.")
+        }
+        const hashedPassword = await bcrypt.hash(userSignUpInput.password, 12)
+        const newUser = new UserModel({ username: userSignUpInput.username, password: hashedPassword });
+        // console.log("user--->", newUser);
+        const savedUser = await newUser.save();
+        const token = newToken(savedUser)
         return {
-          _id: savedUser._id,
+          //this keys should match to the output keys in the signup mutation Auth
+          id: savedUser._id,
           username: savedUser.username,
+          tokenExpire: 60,
+          token
         };
+      
+
       } catch (err) {
         console.log(err);
       }
