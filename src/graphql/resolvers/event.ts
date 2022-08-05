229a43b6dd45486b2bfb07a8af87e9811749baa3
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2022-08-05 10:39:59 +0200

    update event resolver

--- src/graphql/resolvers/event.ts
+++ src/graphql/resolvers/event.ts
@@ -33,6 +33,41 @@ export const eventResolvers = {
                 console.log(err)
             }
         },
-        
+        updateEvent: async(_:any, {event}: CreateEvent, ctx: any) => {
+            try {
+                console.log("event--", event)
+                const user = await AuthMiddleware(ctx)
+                if(!user){
+                    throw new AuthenticationError('Unauthenticated');
+                }
+                const userEvent = await EventModel.findOne({_id: event.id, createdBy: user._id})
+                if(!userEvent){
+                    throw new AuthenticationError(`${event.id} not Found`);
+                }else {
+                    const updatedEvent = await EventModel.findOneAndUpdate({_id: event.id, createdBy: user._id}, {
+                        title: event.title,
+                        start: event.start,
+                        end: event.end,
+                        description: event.description,
+                        isPrivate: event.isPrivate,
+                    },{new: true}
+                    )
+
+                    return {
+                        _id: updatedEvent?._id,
+                        title: updatedEvent?.title,
+                        start: updatedEvent?.start,
+                        end:updatedEvent?.end,
+                        description: updatedEvent?.description,
+                        isPrivate: updatedEvent?.isPrivate,
+                        url: updatedEvent?.url,
+                        createdBy: updatedEvent?.createdBy
+                    }
+                }
+
+            } catch (err) {
+                console.log(err)
+            }
+        }
     }
 }
