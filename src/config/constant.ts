7262902fa73a99d955b5b85e5a42bf45e138b182
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2022-08-05 20:20:56 +0200

    Update constant.ts

--- src/config/constant.ts
+++ src/config/constant.ts
@@ -3,6 +3,7 @@ export const constant = {
   PORT: process.env.PORT || 4000,
   URI: process.env.URL || 'http://localhost:3000',
   MONGODB_URL:
-    process.env.MONGODB_URL || 'mongodb://127.0.0.1:27017/EventScheduler',
+    process.env.MONGODB_URL ,
   JWT_SECRET: process.env.JWT_SECRET || 'dog**&&unning***GrassyField$$$Sunny%%%ay!!!',
 }
+
